56274b759e33210fc9bca873ee3abd84f0647c4e
icespirite <icespirite@yandex.ru>
2019-03-03 20:00:17 +0300

    new service

--- src/main/java/software/engineering/task/Application.java
+++ src/main/java/software/engineering/task/Application.java
@@ -1,13 +1,60 @@
 package software.engineering.task;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * This is entry point to the program
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        System.out.println("restTemplate = " + restTemplate);
+        return restTemplate;
+    }
+
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConFactory
+                = new JedisConnectionFactory();
+//        jedisConFactory.setHostName("localhost");
+//        jedisConFactory.setPort(6379);
+        return jedisConFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+
+        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+
+        return template;
+    }
+
     /**
      * Main method launch server
      *

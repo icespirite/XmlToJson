7258ffc5f60f0d28536bb27ce620c5a42017acdf
icespirite <icespirite@yandex.ru>
2019-03-05 14:34:57 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
-        plugins {
-            java
-            id("org.springframework.boot") version "2.1.1.RELEASE"
-            id("io.spring.dependency-management") version "1.0.6.RELEASE"
-            id("com.palantir.docker") version "0.20.1"
-        }
+plugins {
+    java
+    id("org.springframework.boot") version "2.1.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+    id("com.palantir.docker") version "0.20.1"
+}
 
 group = "software.engineering.task"
 version = "1.0-SNAPSHOT"
@@ -27,24 +27,21 @@ dependencies {
     testCompile("junit", "junit", "4.12")
 }
 
-
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
-tasks {
-    register("unpack", Copy::class) {
-        dependsOn(tasks.named<BootJar>("bootJar"));
+tasks.register<Copy>("unpack") {
+    dependsOn(tasks.named<BootJar>("bootJar"));
 
-        from(zipTree(tasks.named<BootJar>("bootJar").get().outputs.files.singleFile))
-        into("build/dependency")
-    }
+    from(zipTree(tasks.named<BootJar>("bootJar").get().outputs.files.singleFile))
+    into("build/dependency")
 }
 
 docker {
     name = tasks.named<BootJar>("bootJar").get().baseName
-    copySpec.from(tasks["unpack"].outputs).into("dependency")
-    buildArgs(mapOf("DEPENDENCY" to "dependency"))
+    copySpec.from(tasks["unpack"].outputs).into("dependency2121")
+    buildArgs(mapOf("DEPENDENCY" to "dependency2121"))
 }
 
 tasks["build"].finalizedBy(tasks["docker"])
